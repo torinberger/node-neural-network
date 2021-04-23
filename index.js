420cb9b0fe7f5941977b6b7520aab8807f8f997b
torinberger <torinberger@torinberger.com>
2021-04-23 14:55:38 +1000

    Neural network can now mutate

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const NOFINPUTS = 16;
-const NOFHIDDENLAYERS = 1;
-const NOFHIDDENLAYERNODES = [3];
+const NOFHIDDENLAYERS = 2;
+const NOFHIDDENLAYERNODES = 3;
 const NOFOUTPUTS = 4;
 
 function sigmoid(t) {
@@ -15,8 +15,17 @@ function sumWithWeighs(inputs, weights) {
   return count;
 }
 
+function randomInRange(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
 function randomWeight() {
   return Math.random() * (1 - (-1)) + -1;
+}
+
+function chanceToBool(chance) {
+  let choice = randomInRange(0, 100);
+  return choice <= chance ? true : false;
 }
 
 const InputNode = function () {
@@ -55,17 +64,39 @@ const InputLayer = function (nOfInputs) {
   return this;
 }
 
-const Node = function (nOfNodesInLastLayer) {
+const Node = function (nOfNodesInPreviousLayer) {
   this.weights = [];
   this.value = null;
 
-  for(let i = 0; i <= nOfNodesInLastLayer; i += 1) {
+  for(let i = 0; i <= nOfNodesInPreviousLayer; i += 1) {
     this.weights[i] = randomWeight();
   }
 
   this.propagate = function (inputs) {
     this.value = sigmoid(sumWithWeighs(inputs, this.weights));
     return this.value;
+  }
+
+  this.updatenOfNodesInPreviousLayer = function (newNodesInPreviousLayer) {
+    if (newNodesInPreviousLayer > this.weights.length) {
+      this.weights.push(randomWeight());
+    } else if(newNodesInPreviousLayer < this.weights.length) {
+      this.weights.pop();
+    }
+  }
+
+  this.mutateWeight = function (weightChange) {
+    let increaseWeight = chanceToBool(50);
+    let selectWeight = randomInRange(0, this.weights.length - 1);
+    if (increaseWeight) {
+      console.log('from', this.weights[selectWeight]);
+      this.weights[selectWeight] += weightChange;
+      console.log('to', this.weights[selectWeight]);
+    } else {
+      console.log('from', this.weights[selectWeight]);
+      this.weights[selectWeight] -= weightChange;
+      console.log('to', this.weights[selectWeight]);
+    }
   }
 
   return this;
@@ -96,19 +127,33 @@ const HiddenLayer = function (nOfNodes, nOfNodesInPreviousLayer) {
     return this.getNodeValues();
   }
 
+  this.updatenOfNodesInPreviousLayer = function (newNodesInPreviousLayer) {
+    for(let i = 0; i < this.nodes.length; i += 1) {
+      this.nodes[i].updatenOfNodesInPreviousLayer(newNodesInPreviousLayer);
+    }
+  }
+
+  this.addNode = function (nOfNodesInPreviousLayer) {
+    this.nodes.push(new Node(nOfNodesInPreviousLayer));
+  }
+
+  this.removeNode = function () {
+    this.nodes.pop();
+  }
+
   return this;
 }
 
 const Network = function (nOfInputs, nOfHiddenLayers, nOfHiddenLayerNodes, nOfOutputNodes) {
   this.inputLayer = new InputLayer(nOfInputs);
   this.hiddenLayers = [];
-  this.outputLayer = new HiddenLayer(nOfOutputNodes, nOfHiddenLayerNodes[nOfHiddenLayerNodes.length - 1]);
+  this.outputLayer = new HiddenLayer(nOfOutputNodes, nOfHiddenLayerNodes);
 
   for (var i = 0; i < nOfHiddenLayers; i++) {
     if (i === 0) { // if first hidden layer, use input layer as previous layer
-      this.hiddenLayers[i] = new HiddenLayer(nOfHiddenLayerNodes[i], nOfInputs)
+      this.hiddenLayers[i] = new HiddenLayer(nOfHiddenLayerNodes, nOfInputs)
     } else { // if not first hidden layer, use last hidden layer as previous layer
-      this.hiddenLayers[i] = new HiddenLayer(nOfHiddenLayerNodes[i], nOfHiddenLayerNodes[i-1]);
+      this.hiddenLayers[i] = new HiddenLayer(nOfHiddenLayerNodes, this.hiddenLayers[i -1].nodes.length);
     }
   }
 
@@ -125,10 +170,58 @@ const Network = function (nOfInputs, nOfHiddenLayers, nOfHiddenLayerNodes, nOfOu
     return outputLayerValues;
   }
 
+  this.mutate = function (layerMutateChance, nodeMutateChance, weightMutateChance, addChance, weightChange) {
+    if(chanceToBool(layerMutateChance)) {
+      console.log('Mutating layer');
+      let addLayer = chanceToBool(addChance);
+      if (addLayer) {
+        console.log('adding');
+        this.hiddenLayers.push(new HiddenLayer(nOfHiddenLayerNodes, this.hiddenLayers[this.hiddenLayers.length - 1].nodes.length));
+        this.outputLayer.updatenOfNodesInPreviousLayer(nOfHiddenLayerNodes);
+      } else {
+        console.log('removing');
+        this.hiddenLayers.pop();
+        this.outputLayer.updatenOfNodesInPreviousLayer(this.hiddenLayers[this.hiddenLayers.length - 1].nodes.length);
+      }
+    } if (chanceToBool(nodeMutateChance)) {
+      console.log('Mutating node');
+      let addNode = chanceToBool(addChance);
+      if (addNode) {
+        console.log('adding');
+        let selectLayer = randomInRange(0, this.hiddenLayers.length - 1);
+        this.hiddenLayers[selectLayer].addNode(this.hiddenLayers[this.hiddenLayers.length - 1].nodes.length);
+        if (selectLayer === this.hiddenLayers.length - 1) {
+          this.outputLayer.updatenOfNodesInPreviousLayer(this.hiddenLayers[selectLayer].nodes.length);
+        } else {
+          this.hiddenLayers[selectLayer + 1].updatenOfNodesInPreviousLayer(this.hiddenLayers[selectLayer].nodes.length);
+        }
+      } else {
+        console.log('removing');
+        let selectLayer = randomInRange(0, this.hiddenLayers.length - 1);
+        this.hiddenLayers[selectLayer].removeNode();
+        if (selectLayer === this.hiddenLayers.length - 1) {
+          this.outputLayer.updatenOfNodesInPreviousLayer(this.hiddenLayers[selectLayer].nodes.length);
+        } else {
+          this.hiddenLayers[selectLayer + 1].updatenOfNodesInPreviousLayer(this.hiddenLayers[selectLayer].nodes.length);
+        }
+      }
+    } if (chanceToBool(weightMutateChance)) {
+      console.log('Mutating weight');
+
+      let selectLayer = randomInRange(0, this.hiddenLayers.length - 1);
+      let selectNode = randomInRange(0, this.hiddenLayers[selectLayer].nodes.length - 1);
+      this.hiddenLayers[selectLayer].nodes[selectNode].mutateWeight(weightChange);
+    }
+  }
+
   return this;
 }
 
 const testNetwork = new Network(NOFINPUTS, NOFHIDDENLAYERS, NOFHIDDENLAYERNODES, NOFOUTPUTS);
 
 console.log('--- Propagating Network Layers');
+console.log(testNetwork.propagate([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]));
+console.log(testNetwork.hiddenLayers);
+testNetwork.mutate(20, 20, 50, 50, 0.1);
+console.log(testNetwork.hiddenLayers);
 console.log(testNetwork.propagate([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]));
